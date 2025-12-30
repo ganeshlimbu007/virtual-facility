d9d67475da8a267e6f9f66101905f8e9984569f1
ganeshlimbu007 <57285671+ganeshlimbu007@users.noreply.github.com>
2025-12-30 15:36:56 +0545

    save all

--- apps/workflow-services/src/workflows/workflows.module.ts
+++ apps/workflow-services/src/workflows/workflows.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { WorkflowsService } from './workflows.service';
 import { WorkflowsController } from './workflows.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    WorkflowsModule,
+  ],
   controllers: [WorkflowsController],
   providers: [WorkflowsService],
 })
